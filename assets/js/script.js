0386e791924488fd019e32452c10efec738f0c76
Owni <120651078+mano6195@users.noreply.github.com>
2024-03-10 17:34:49 +0100

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,4 @@
+document.getElementById('register-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Gracias por registrarte, ahora ya formas parte de ViperZ! | discord.gg/viperz');
+});
